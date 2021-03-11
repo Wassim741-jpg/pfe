aff1626fb915b422d1883cd15876047e3456db15
wcherif <wcherif@iobeya.com>
2021-03-11 10:42:23 +0100

    second commit

--- App.js
+++ App.js
@@ -1,43 +1,91 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet ,
-    Text,
-    View,
-    ImageBackground,
-    Dimensions,
-    TouchableOpacity
-} from 'react-native';
+import * as React from 'react';
+import { StyleSheet ,Button,ImageBackground, View, Text , Dimensions, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import bgImage from './Images/background.png'
+import bgImage2 from './Images/background2.jpg'
 import {TextInput} from "react-native-gesture-handler";
 const  { width: WIDTH } =Dimensions.get('window')
-export default class Example extends Component {
-    render() {
-        return (
-            <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Username'}
-                        placeholderTextColor={'rgba(255,255,255,0.7'}
-                        underlineColorAndroid='transparent'
-                    />
-                </View>
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder={'Password'}
-                        placeholderTextColor={'rgba(255,255,255,0.7'}
-                        underlineColorAndroid='transparent'
-                    />
-                </View>
-                <TouchableOpacity style={styles.btnLogin}>
-                    <Text style={styles.text}>Login</Text>
-                </TouchableOpacity>
-            </ImageBackground>
-                );
-    }
+
+
+function HomeScreen({ navigation }) {
+    return (
+        <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Username'}
+                    placeholderTextColor={'rgba(255,255,255,0.7'}
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder={'Password'}
+                    placeholderTextColor={'rgba(255,255,255,0.7'}
+                    underlineColorAndroid='transparent'
+                />
+            </View>
+            <View >
+                <Button
+                    title="Login"
+                    onPress={() => navigation.navigate('Details')}
+                />
+            </View>
+        </ImageBackground>
+
+    );
 }
 
+function DetailsScreen() {
+    return (
+        <ImageBackground source={bgImage2} style={styles.backgroundContainer}>
+        <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.input}
+                placeholder={'URL API'}
+                placeholderTextColor={'rgba(255,255,255,0.7'}
+                underlineColorAndroid='transparent'
+            />
+        </View>
+    <View style={styles.inputContainer}>
+        <TextInput
+            style={styles.input}
+            placeholder={'BORD ID'}
+            placeholderTextColor={'rgba(255,255,255,0.7'}
+            underlineColorAndroid='transparent'
+        />
+        </View>
+            <View >
+                <Button
+                    title="Start Synch"
+                />
+            </View>
+            <View >
+                <Button
+                    title="Stop Synch"
+                />
+            </View>
+        </ImageBackground>
+    );
+}
+
+const Stack = createStackNavigator();
+
+export default class Example extends React.Component {
+    render() {
+
+        return (
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home">
+                    <Stack.Screen name="Home" component={HomeScreen}/>
+                    <Stack.Screen name="Details" component={DetailsScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
+}
 const styles =  StyleSheet.create({
     backgroundContainer: {
         flex: 1,
